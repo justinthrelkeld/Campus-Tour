89fec228ce20bf719949b7644462e47936d5d4eb
Justin Threlkeld <justin.threlkeld@gmail.com>
2013-04-06 11:13:05 -0500

    json formatted properly

--- js/map.js
+++ js/map.js
@@ -3,16 +3,19 @@ var map;
 
 var points = /*jQuery.getJSON("landmarks.json");*/
 
-{"KOM1": {
+{0: {
+  "id": "KOM1",
   "lat": 35.849631,
   "lng": -86.368654,
   "title": "Kirksey Old Main",
+  "description": "This is the KOM",
   "media": {
     "type": "image",
     "src": "kom1.jpg"
   }
 },
-"stop1": {
+1: {
+  "id": "stop1",
   "lat": 35.849,
   "lng": -86.362,
   "title": "Stop 1",
@@ -21,7 +24,8 @@ var points = /*jQuery.getJSON("landmarks.json");*/
     "src": "kom1.jpg"
   }
 },
-"stop2": {
+2: {
+  "id": "stop2",
   "lat": 35.846,
   "lng": -86.362,
   "title": "Stop 2",
@@ -30,55 +34,56 @@ var points = /*jQuery.getJSON("landmarks.json");*/
     "src": "kom1.jpg"
   }
 },
-"stop3": {
+3: {
+  "id": "stop3",
   "lat": 35.852,
   "lng": -86.362,
   "title": "Stop 3"
 }
-}};
+};
 
-alert(points.landmarks.KOM1.lat);
+alert(points[0].lat);
 
 function initialize() {
   console.log('initializing Google map');
 
   var defaultLatlng = new google.maps.LatLng(35.849057,-86.362374);
   var thisLatlng = defaultLatlng;
-  
-//          function getLocation()
-//          {
-//            if (navigator.geolocation)
-//            {
-//              navigator.geolocation.getCurrentPosition(userPosition,showError);
-//            }
-//            else{console.log("Geolocation is not supported by this browser.");}
-//          }
-//
-//          function userPosition(position)
-//          {
-//            var userLatlng=position.coords.latitude+","+position.coords.longitude;
-//            thisLatlng=userLatlng;
-//          }
-//
-//          function showError(error)
-//         {
-//            switch(error.code) 
-//            {
-//              case error.PERMISSION_DENIED:
-//              console.log("User denied the request for Geolocation.")
-//              break;
-//              case error.POSITION_UNAVAILABLE:
-//              console.log("Location information is unavailable.")
-//              break;
-//              case error.TIMEOUT:
-//              console.log("The request to get user location timed out.")
-//              break;
-//              case error.UNKNOWN_ERROR:
-//              console.log("An unknown error occurred.")
-//              break;
-//            }
-//          }
-//          getLocation();
+
+/*  function getLocation()
+  {
+    if (navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition(userPosition,showError);
+    }
+    else{console.log("Geolocation is not supported by this browser.");}
+  }
+
+  function userPosition(position)
+  {
+   var userLatlng=position.coords.latitude+","+position.coords.longitude;
+   thisLatlng=userLatlng;
+ }
+
+ function showError(error)
+ {
+  switch(error.code) 
+  {
+    case error.PERMISSION_DENIED:
+    console.log("User denied the request for Geolocation.")
+    break;
+    case error.POSITION_UNAVAILABLE:
+    console.log("Location information is unavailable.")
+    break;
+    case error.TIMEOUT:
+    console.log("The request to get user location timed out.")
+    break;
+    case error.UNKNOWN_ERROR:
+    console.log("An unknown error occurred.")
+    break;
+  }
+}
+getLocation();*/
 
 var mapOptions = {
   zoom: 15,
@@ -96,46 +101,46 @@ google.maps.event.addListener(map, 'click', function(e) {
   map.setZoom(15);
 })
 
-        // Add markers to the map from array points.
-        for (var i = 0; i < points.length; i++) {
-          var location = new google.maps.LatLng(points[i][0],points[i][1]);
-          var title = points[i][2];
-          var marker = new google.maps.Marker({
-            position: location,
-            title: title,
-            map: map
-          });
-          attachPoints(marker, i);
-        }
+// Add markers to the map from array points.
+for (var i = 0; i < points.length; i++) {
+  var location = new google.maps.LatLng(points[i][0],points[i][1]);
+  var title = points[i][2];
+  var marker = new google.maps.Marker({
+    position: location,
+    title: title,
+    map: map
+  });
+  attachPoints(marker, i);
+}
 
-        $('#mapDrawer').waypoint(function() {
-          console.log('Top collided');
-        }, {offset: 1});
+$('#mapDrawer').waypoint(function() {
+  console.log('Top collided');
+}, {offset: 1});
 
-        $('#mapDrawer').waypoint(function() {
-          console.log('bottom collided');
-        }, { 
-          offset: function() {
-            return -$(this).height();
-          }});
+$('#mapDrawer').waypoint(function() {
+  console.log('bottom collided');
+}, { 
+  offset: function() {
+    return -$(this).height();
+  }});
 
-        originalMapHeight = $('#mapDrawer').height(); 
+originalMapHeight = $('#mapDrawer').height(); 
 
-        $('.nav').on('click', function() {
-          $.smoothScroll({
-            scrollTarget: '#top'
-          });
-          return false;
-        });
-        $('').waypoint(function(direction) {
-          alert('Top of thing hit top of viewport.' + direction);
-        });
+$('.nav').on('click', function() {
+  $.smoothScroll({
+    scrollTarget: '#top'
+  });
+  return false;
+});
+$('').waypoint(function(direction) {
+  alert('Top of thing hit top of viewport.' + direction);
+});
 
-      };
+};
 
-      function attachPoints(marker, number) {
-        var currentTourStop = points[number][3];
-        google.maps.event.addListener(marker, 'click', function() {
+function attachPoints(marker, number) {
+  var currentTourStop = points[number][3];
+  google.maps.event.addListener(marker, 'click', function() {
           //goto point logic
           console.log('going to ' + currentTourStop);
           //window.location.assign('#' + currentTourStop);
@@ -150,7 +155,7 @@ google.maps.event.addListener(map, 'click', function(e) {
           });
           $('#mapDrawer').removeClass('open', 400);
         });
-      };
+};
           //window.location.assign('#' + currentTourStop);
           $.smoothScroll({
             scrollTarget: '#' + currentTourStop
