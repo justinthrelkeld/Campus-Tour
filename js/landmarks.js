a0824edb74d0acfcd3cfcbcaaabe9aeb4df05262
Justin Threlkeld <justin.threlkeld@gmail.com>
2013-04-16 10:42:37 -0500

    minor style adjustments (media objects and footer)

--- js/landmarks.js
+++ js/landmarks.js
@@ -4,12 +4,12 @@ var points =
   "lat": 35.849631,
   "lng": -86.368654,
   "title": "Kirksey Old Main",
-  "description": "This is the KOM",
+  "description": "This is the KOM. You will wear a helmet on your bike. She's gonna make it someday. Chandler! yeah. Wait... Wait... Wait... hahaha! There's nothing down there. That one? I'm waiting. What did you say? I said wait. No, you. I didn't say anything. Look at the puppies!! Too funny! Well I didn't know.",
   "media": 
   [{
     "title": "Media Item 1",
     "type": "image",
-    "src": "kom1.jpg",
+    "thumb": "http://mtsu.edu/images/01-mtsu-art.gif",
     "url": "http://mtsu.edu/"
   }]
 },
